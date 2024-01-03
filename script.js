292a196d3fafeb2c56ba2c052295bea7631a0d7c
ssabirovo <muhiddinsobirov957@gmail.com>
2024-01-03 18:00:34 +0500

    feat: added docs pages and pagination element

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("check");
+let dropdown = document.getElementById("dropdown");
+
+btn.addEventListener("click", () => {
+  dropdown.classList.toggle("dropdown-show");
+});
